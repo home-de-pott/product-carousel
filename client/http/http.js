40d799b567d88e658997d0bca7e4d337a38b677a
Brian Embry <b.a.embry@gmail.com>
2019-07-15 16:15:42 -0500

    Add some console.logs to trouble shoot getRelatedProducts

--- client/http/http.js
+++ client/http/http.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 const http = {
   productData: {
     get: async function(id) {
-      console.log('Getting', id, process.env);
       try {
         const response = await axios.get(
           'http://ec2-18-221-151-249.us-east-2.compute.amazonaws.com/product-data/' +
             id
         );
+        console.log('Response', response);
         return response.data;
       } catch (error) {
         console.error(error);
